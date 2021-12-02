5e1994ccda434392a76fc0fb440d3c6a04a1b9f5
Evgeniy Lebedev <lebedev1537@gmail.com>
2021-12-03 01:04:13 +0400

    Added web module

---
+++ web/src/platform/platform.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserComponent } from './browser/browser.component';
+
+@NgModule({
+  declarations: [
+    BrowserComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PlatformModule { }
